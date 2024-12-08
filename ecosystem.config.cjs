9ab806f1dae9c2be38a73bc0f4de899271985d57
pimsras <julian.trifu69@gmail.com>
2024-12-08 17:17:55 +0100

    fix pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+      apps: [
+        {
+          name: "kodyborg",
+          script: "bun",
+          args: "run start",
+          watch: true,
+        },
+      ],
+    };
+    
